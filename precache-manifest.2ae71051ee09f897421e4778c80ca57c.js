4a37447a615c168f715f956facddc346885b7e0c
Giao Nguyen <giao.nguyenn@yahoo.com>
2019-10-10 14:06:36 -0500

    Updates

---
+++ precache-manifest.2ae71051ee09f897421e4778c80ca57c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2220c73b06b932a71c32139fa33f5d3",
+    "url": "/light-switch/index.html"
+  },
+  {
+    "revision": "fbea018f99ce417928cc",
+    "url": "/light-switch/static/css/main.8e11906b.chunk.css"
+  },
+  {
+    "revision": "20e880cd45e0dead80b8",
+    "url": "/light-switch/static/js/2.f08d2512.chunk.js"
+  },
+  {
+    "revision": "fbea018f99ce417928cc",
+    "url": "/light-switch/static/js/main.e7992d65.chunk.js"
+  },
+  {
+    "revision": "234b618223fe1fd087a5",
+    "url": "/light-switch/static/js/runtime-main.d1df5045.js"
+  }
+]);
